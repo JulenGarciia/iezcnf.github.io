8dc5f0cb8a8e1b9a7949054e65c80e0182980b73
imaleex <alexcortes90@gmail.com>
2020-04-21 11:48:23 +0200

    Create pbis.sh

---
+++ egibide/pbis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+function Script() {
+tmpfile=$(mktemp tmp.XXXXXXX --tmpdir=/tmp 2>/dev/null) || tmpfile=/tmp/test$$
+
+dialog          --backtitle "Pbis Grupo6"  \
+                --title "Pbis Grupo6" \
+                --clear \
+                --menu "Elige una opcion:" 15 50 4 \
+                1 "Instalar y configurar Pbis" \
+                2 "Añadir usuarios a el archivo sudoers"  2> ${tmpfile}
+
+if [ "$?" -eq "0" ]; then
+
+grep -o 1 $tmpfile > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+	resultdns=`[ "$(dig +short -t a grupo6.local)" ] && echo "responde"`
+	if ["$resultdns" -eq "responde"]; then
+      clear
+      bash <(wget -O - 'https://github.com/BeyondTrust/pbis-open/releases/download/9.1.0/pbis-open-9.1.0.551.linux.x86_64.deb.sh')
+      clearu78
+	  cd /opt/pbis/bin/
+	  echo "Introduce contraseña de el usuario Administrador"
+	  read Contrasena
+	  /opt/pbis/bin/domainjoin-cli join grupo6.local Administrador $Contrasena
+      clear
+	  /opt/pbis/bin/domainjoin-cli query
+	  echo 'greeter-show-manual-login=true' >> /usr/share/lightdm.conf.d/50-ubuntu.conf
+	  clear
+	  /opt/pbis/bin/config LoginShellTemplate /bin/bash
+      Script
+	  fi
+	  echo 'La red no esta configurada de manera correcta, comprueba el servidor DNS'
+    fi
+
+grep -o 2 $tmpfile > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+      clear
+    Script
+    fi
+
+else
+    clear
+    echo "Operacion Cancelada"
+
+fi
+}
+apt update
+apt install dialog -y
+clear
+Script
